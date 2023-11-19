f1d7fe975f06ce3fc5745a18e0af735f287216c9
Michele Shayane <106595533+michele09082007@users.noreply.github.com>
2023-11-19 04:02:35 -0300

    d

--- mains.js
+++ mains.js
@@ -33,6 +33,10 @@ function tocaSomPsh() {
 }
 document.querySelector ('.tecla_psh').onclick= tocaSomPsh;
 
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector ('.tecla_tic').onclick= tocaSomTic
 
 function tocaSomTom() {
     document.querySelector('#som_tecla_tom').play();
